da356cf1dfa0f6a8e859b98a201688bbb0bde715
bandurskiego78 <bandurskiego78@gmail.com>
2015-03-22 19:51:21 +0100

    created mean value object.

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,11 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+# I. Added steps
+# 1. Initialize m object as mean value equal NULL
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
 
 }
 
